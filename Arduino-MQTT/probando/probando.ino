3156c37ee09344d330a10bb57c04cb80113415ec
Josessantos5034 <josesantos5034102@gmail.com>
2021-07-06 11:20:31 -0600

    variables reparadas, funcionado con el serial available

--- Arduino-MQTT/probando/probando.ino
+++ Arduino-MQTT/probando/probando.ino
@@ -1,6 +1,6 @@
 #define posicion_mandarina 0
-#define posicion_uva_verde 120
-#define posicion_uva_roja 240
+#define posicion_pera 120
+#define posicion_uva 240
 int GradoActual = 0;
 int GradoDeseado = 0;
 
@@ -11,16 +11,16 @@ const int P_Step = 19;
 const int P_Dir = 18;
 const int led_estado = 13;
 const int led_mandarina = 12;
-const int led_uva_verde = 14;
-const int led_uva_roja = 27;
+const int led_pera = 14;
+const int led_uva = 27;
 
 void setup() {
   pinMode(P_Step, OUTPUT);
   pinMode(P_Dir, OUTPUT);
   pinMode(led_estado, OUTPUT);
   pinMode(led_mandarina, OUTPUT);
-  pinMode(led_uva_verde, OUTPUT);
-  pinMode(led_uva_roja, OUTPUT);
+  pinMode(led_pera, OUTPUT);
+  pinMode(led_uva, OUTPUT);
   pinMode(BotonIzquierda, INPUT);
   pinMode(BotonDerecha, INPUT);
   Serial.begin(9600);
@@ -46,7 +46,7 @@ void ActualizarPosicionMotores() {
 }
 
 void SubirGrado() {
-  Serial.println("Moviendo Abajo");
+  Serial.println("Moviendo Arriba");
   GradoActual++;
   MoverMotor(1);
 }
@@ -79,8 +79,8 @@ void ActualizarBotones() {
     GradoActual = 0;
     GradoDeseado = 0;
     digitalWrite(led_mandarina, LOW);
-    digitalWrite(led_uva_verde, LOW);
-    digitalWrite(led_uva_roja, LOW);
+    digitalWrite(led_pera, LOW);
+    digitalWrite(led_uva, LOW);
   }
   if (digitalRead(BotonDerecha)) {
     Serial.println("Reiniciando hacia la derecha");
@@ -88,36 +88,34 @@ void ActualizarBotones() {
     GradoActual = 0;
     GradoDeseado = 0;
     digitalWrite(led_mandarina, LOW);
-    digitalWrite(led_uva_verde, LOW);
-    digitalWrite(led_uva_roja, LOW);
+    digitalWrite(led_pera, LOW);
+    digitalWrite(led_uva, LOW);
   }
 }
-
-
 
 void DetectarMensaje() {
   if (Serial.available()) {
     char Letra = Serial.read();
     digitalWrite(led_estado, 0);
     if (Letra == 'M') {
-      GradoDeseado = posicion_manzana;
-      Serial.println("Cambiando a Manzana");
-      digitalWrite(led_manzana, HIGH);
+      GradoDeseado = posicion_mandarina;
+      Serial.println("Cambiando a Mandarina");
+      digitalWrite(led_mandarina, HIGH);
       digitalWrite(led_pera, LOW);
-      digitalWrite(led_naranja, LOW);
+      digitalWrite(led_uva, LOW);
     }
-    else if (Letra == 'J') {
-      GradoDeseado = posicion_naranja;
-      Serial.println("Cambiando a Naranja");
-      digitalWrite(led_naranja, HIGH);
-      digitalWrite(led_pera, LOW);
-      digitalWrite(led_manzana, LOW);
-    } else if (Letra == 'P') {
+    else if (Letra == 'P') {
       GradoDeseado = posicion_pera;
       Serial.println("Cambiando a Pera");
       digitalWrite(led_pera, HIGH);
-      digitalWrite(led_naranja, LOW);
-      digitalWrite(led_manzana, LOW);
+      digitalWrite(led_mandarina, LOW);
+      digitalWrite(led_uva, LOW);
+    } else if (Letra == 'U') {
+      GradoDeseado = posicion_uva;
+      Serial.println("Cambiando a Uva");
+      digitalWrite(led_uva, HIGH);
+      digitalWrite(led_pera, LOW);
+      digitalWrite(led_mandarina, LOW);
     }
   }
 }
